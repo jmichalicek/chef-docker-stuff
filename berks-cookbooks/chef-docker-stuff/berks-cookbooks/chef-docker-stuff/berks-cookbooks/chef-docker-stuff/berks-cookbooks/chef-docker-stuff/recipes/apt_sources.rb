570f8fd1dee3414863fc7fe44575de8568188689
Justin Michalicek <jmichalicek@gmail.com>
2016-02-22 22:29:54 -0500

    move stuff around with berks vendor so it all works

---
+++ berks-cookbooks/chef-docker-stuff/berks-cookbooks/chef-docker-stuff/berks-cookbooks/chef-docker-stuff/berks-cookbooks/chef-docker-stuff/recipes/apt_sources.rb
@@ -0,0 +1,14 @@
+# make this more configurable to work on other releases, etc.
+# include_recipe 'apt'
+
+apt_repository 'docker' do
+  uri        'https://apt.dockerproject.org/repo'
+  components ['main', ]
+  distribution 'debian-jessie'
+end
+
+bash 'set_docker_apt_policy' do
+  code <<-EOH
+    apt-cache policy docker-engine
+    EOH
+end 
