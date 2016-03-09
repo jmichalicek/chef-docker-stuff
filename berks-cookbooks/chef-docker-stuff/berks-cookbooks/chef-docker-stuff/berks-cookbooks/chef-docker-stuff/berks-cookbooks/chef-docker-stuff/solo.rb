2c7ffa440fabe0b1c87db826db1eb26aa002eb89
Justin Michalicek <jmichalicek@gmail.com>
2016-03-08 22:47:53 -0500

    really remove weird files from multiple runs of berks vendor

--- berks-cookbooks/chef-docker-stuff/berks-cookbooks/chef-docker-stuff/berks-cookbooks/chef-docker-stuff/berks-cookbooks/chef-docker-stuff/solo.rb
+++
@@ -1,7 +0,0 @@
-Encoding.default_external = "UTF-8"
-
-root = File.absolute_path(File.dirname(__FILE__))
-
-file_cache_path root
-cookbook_path [root + '/berks-cookbooks']
-#role_path root + '/roles'
--- recipes/docker.rb
+++ recipes/docker.rb
@@ -13,8 +13,22 @@ docker_container 'docker_1' do
   port '1234:1234'
 end
 
-docker_container 'django_app' do
-  repo 'debian'
-  port '8000:8000'
-  command 'bash'
+cookbook_file '/tmp/Dockerfile' do
+  source 'Dockerfile'
+  action :create
 end
+
+docker_image 'my_image' do
+  source '/tmp/Dockerfile'
+  action :build
+end
+
+docker_container 'django_app' do
+  repo 'my_image'
+end
+
+#docker_container 'django_app' do
+#  repo 'debian'
+#  port '8000:8000'
+#  command 'bash'
+#end
